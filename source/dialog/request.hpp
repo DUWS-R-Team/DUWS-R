3ac9208c822b963a2e404184ff63a78fb1cafb23
malline <fritogotlayed@gmail.com>
2015-02-15 09:43:26 -0600

    Removed old backup files. Fixed some spelling mistakes and made single TODO for localization.

--- source/dialog/request.hpp
+++ source/dialog/request.hpp
@@ -1,14 +1,11 @@
+// TODO: Localize text in this file.
 class ressourceheader
 {
     idd = -1;
     movingenable = true;
-    //  onLoad = "_this call FRAME_01_Load";
 
     class controls
     {
-        ////////////////////////////////////////////////////////
-        // GUI EDITOR OUTPUT START
-        ////////////////////////////////////////////////////////
         class background_request: RscText
         {
             idc = 1769;
@@ -22,7 +19,7 @@ class ressourceheader
         {
             idc = 1800;
             moving = 1;
-            text = "ASSETS INTERFACE"; //--- ToDo: Localize;
+            text = "ASSETS INTERFACE";
             x = 0 * GUI_GRID_W + GUI_GRID_X;
             y = 9.5 * GUI_GRID_H + GUI_GRID_Y;
             w = 40 * GUI_GRID_W;
@@ -32,7 +29,7 @@ class ressourceheader
         class requestsquadframe: RscFrame
         {
             idc = 1802;
-            text = "SQUAD"; //--- ToDo: Localize;
+            text = "SQUAD";
             x = 13.5 * GUI_GRID_W + GUI_GRID_X;
             y = 13 * GUI_GRID_H + GUI_GRID_Y;
             w = 12.5 * GUI_GRID_W;
@@ -41,7 +38,7 @@ class ressourceheader
         class emptyvehicleframe: RscFrame
         {
             idc = 1803;
-            text = "EMPTY VEHICLE"; //--- ToDo: Localize;
+            text = "EMPTY VEHICLE";
             x = 26.5 * GUI_GRID_W + GUI_GRID_X;
             y = 13 * GUI_GRID_H + GUI_GRID_Y;
             w = 12.5 * GUI_GRID_W;
@@ -50,7 +47,7 @@ class ressourceheader
         class supportunlockframe: RscFrame
         {
             idc = 1804;
-            text = "UNLOCK SUPPORT"; //--- ToDo: Localize;
+            text = "UNLOCK SUPPORT";
             x = 26.5 * GUI_GRID_W + GUI_GRID_X;
             y = 17.5 * GUI_GRID_H + GUI_GRID_Y;
             w = 12.5 * GUI_GRID_W;
@@ -59,7 +56,7 @@ class ressourceheader
         class requestunitframe: RscFrame
         {
             idc = 1801;
-            text = "UNIT"; //--- ToDo: Localize;
+            text = "UNIT";
             x = 0.5 * GUI_GRID_W + GUI_GRID_X;
             y = 13 * GUI_GRID_H + GUI_GRID_Y;
             w = 12.5 * GUI_GRID_W;
@@ -68,18 +65,18 @@ class ressourceheader
         class requestunitsbutton: RscButton
         {
             idc = 1600;
-            text = "REQUEST"; //--- ToDo: Localize;
+            text = "REQUEST";
             x = 1 * GUI_GRID_W + GUI_GRID_X;
             y = 15.5 * GUI_GRID_H + GUI_GRID_Y;
             w = 11.5 * GUI_GRID_W;
             h = 1 * GUI_GRID_H;
             action = "_nil=[]ExecVM ""dialog\request_unit.sqf""";
-            tooltip = "Request a single unit AI to join your own personnal squad";
+            tooltip = "Request a single unit AI to join your own personal squad";
         };
         class requestsquadbutton: RscButton
         {
             idc = 1601;
-            text = "REQUEST"; //--- ToDo: Localize;
+            text = "REQUEST";
             x = 14 * GUI_GRID_W + GUI_GRID_X;
             y = 15.5 * GUI_GRID_H + GUI_GRID_Y;
             w = 11.5 * GUI_GRID_W;
@@ -90,7 +87,7 @@ class ressourceheader
         class requestvehiclebutton: RscButton
         {
             idc = 1602;
-            text = "REQUEST"; //--- ToDo: Localize;
+            text = "REQUEST";
             x = 27 * GUI_GRID_W + GUI_GRID_X;
             y = 15.5 * GUI_GRID_H + GUI_GRID_Y;
             w = 11.5 * GUI_GRID_W;
@@ -101,7 +98,7 @@ class ressourceheader
         class supportunlockbutton: RscButton
         {
             idc = 1603;
-            text = "UNLOCK"; //--- ToDo: Localize;
+            text = "UNLOCK";
             x = 27 * GUI_GRID_W + GUI_GRID_X;
             y = 21.5 * GUI_GRID_H + GUI_GRID_Y;
             w = 11.5 * GUI_GRID_W;
@@ -112,7 +109,7 @@ class ressourceheader
         class exitbutton: RscButton
         {
             idc = 1604;
-            text = "EXIT"; //--- ToDo: Localize;
+            text = "EXIT";
             x = 16 * GUI_GRID_W + GUI_GRID_X;
             y = 23.5 * GUI_GRID_H + GUI_GRID_Y;
             w = 8 * GUI_GRID_W;
@@ -122,7 +119,7 @@ class ressourceheader
         class RscText_1000: RscText
         {
             idc = 1000;
-            text = "CP: XXX"; //--- ToDo: Localize;
+            text = "CP: XXX";
             x = 0.5 * GUI_GRID_W + GUI_GRID_X;
             y = 10.5 * GUI_GRID_H + GUI_GRID_Y;
             w = 4 * GUI_GRID_W;
@@ -132,7 +129,7 @@ class ressourceheader
         class RscText_1001: RscText
         {
             idc = 1001;
-            text = "ZUC:XXX"; //--- ToDo: Localize;
+            text = "ZUC:XXX";
             x = 10.5 * GUI_GRID_W + GUI_GRID_X;
             y = 11 * GUI_GRID_H + GUI_GRID_Y;
             w = 8 * GUI_GRID_W;
@@ -142,53 +139,53 @@ class ressourceheader
         class RscFrame_1805: RscFrame
         {
             idc = 1805;
-            text = "Command points"; //--- ToDo: Localize;
+            text = "Command points";
             x = 0.5 * GUI_GRID_W + GUI_GRID_X;
             y = 10.5 * GUI_GRID_H + GUI_GRID_Y;
             w = 9 * GUI_GRID_W;
             h = 2 * GUI_GRID_H;
-            tooltip = "Command points available"; //--- ToDo: Localize;
+            tooltip = "Command points available";
         };
         class RscFrame_1806: RscFrame
         {
             idc = 1806;
-            text = "Zones under control"; //--- ToDo: Localize;
+            text = "Zones under control";
             x = 10.5 * GUI_GRID_W + GUI_GRID_X;
             y = 10.5 * GUI_GRID_H + GUI_GRID_Y;
             w = 8.5 * GUI_GRID_W;
             h = 2 * GUI_GRID_H;
-            tooltip = "Number of zones you have captured on this island"; //--- ToDo: Localize;
+            tooltip = "Number of zones you have captured on this island";
         };
         class RscButton_1605: RscButton
         {
             idc = 1605;
-            text = "SELECT SIDE MISSION"; //--- ToDo: Localize;
+            text = "SELECT SIDE MISSION";
             x = 28.5 * GUI_GRID_W + GUI_GRID_X;
             y = 11 * GUI_GRID_H + GUI_GRID_Y;
             w = 10.5 * GUI_GRID_W;
             h = 1.5 * GUI_GRID_H;
             action = "_derp = [] execVM 'missions\stratmap.sqf'; closeDialog 0";
-            tooltip = "Select a random side mission. Accomplishing a side mission gives you CP."; //--- ToDo: Localize;
+            tooltip = "Select a random side mission. Accomplishing a side mission gives you CP.";
         };
         class ap_mainframe: RscFrame
         {
             idc = 1807;
-            text = "Army Power"; //--- ToDo: Localize;
+            text = "Army Power";
             x = 19.5 * GUI_GRID_W + GUI_GRID_X;
             y = 10.5 * GUI_GRID_H + GUI_GRID_Y;
             w = 8.5 * GUI_GRID_W;
             h = 2 * GUI_GRID_H;
-            tooltip = "Actual strenght of BLUFOR presence"; //--- ToDo: Localize;
+            tooltip = "Actual strength of BLUFOR presence";
         };
         class ap_text: RscText
         {
             idc = 1002;
-            text = "000"; //--- ToDo: Localize;
+            text = "000";
             x = 19.5 * GUI_GRID_W + GUI_GRID_X;
             y = 11 * GUI_GRID_H + GUI_GRID_Y;
             w = 6 * GUI_GRID_W;
             h = 1.5 * GUI_GRID_H;
-            tooltip = "Actual strenght of BLUFOR presence"; //--- ToDo: Localize;
+            tooltip = "Actual strength of BLUFOR presence";
             colorText[] = {1,1,1,1};
         };
         class unitlistbox: RscCombo
@@ -230,7 +227,7 @@ class ressourceheader
         class convertCPbutton: RscButton
         {
             idc = 1627;
-            text = "Convert 10 CP into 15 AP"; //--- ToDo: Localize;
+            text = "Convert 10 CP into 15 AP";
             x = 1 * GUI_GRID_W + GUI_GRID_X;
             y = 20.5 * GUI_GRID_H + GUI_GRID_Y;
             w = 11.5 * GUI_GRID_W;
@@ -241,7 +238,7 @@ class ressourceheader
         class recruit_operative_button: RscButton
         {
             idc = 1347;
-            text = "Recruit special operatives"; //--- ToDo: Localize;
+            text = "Recruit special operatives";
             x = 1 * GUI_GRID_W + GUI_GRID_X;
             y = 18 * GUI_GRID_H + GUI_GRID_Y;
             w = 11.5 * GUI_GRID_W;
@@ -249,8 +246,5 @@ class ressourceheader
             action = "execVM ""dialog\operative\operator_open.sqf""";
             tooltip = "Recruit special units that will stay during the entire campaign";
         };
-        ////////////////////////////////////////////////////////
-        // GUI EDITOR OUTPUT END
-        ////////////////////////////////////////////////////////
     };
 };
