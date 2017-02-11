27cba2fc959d132efe302d750c914e161ace4566
UltraNoobian <caibrenden@yahoo.com.au>
2017-02-11 16:43:26 +1100

    Fixed type

--- source/params.hpp
+++ source/params.hpp
@@ -6,7 +6,7 @@ class Params {
         default = true;
     };
 
-    class DeatPenalty {
+    class DeathPenalty {
         title = "CP death penalty";
         values[] = {1,2,3,4,5,6,7,8,9,10,0};
         texts[] = {"1","2","3","4","5","6","7","8","9","10","No penalty"};
